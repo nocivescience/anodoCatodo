e03263f6c3e764098a57752b0614c455fa166aa0
ricardo <nocive.science@gmail.com>
2024-03-23 02:55:02 -0300

    hecho el asunto del while true

---
+++ practicaWhile.py
@@ -0,0 +1,14 @@
+contador= 1
+while True:
+    print(contador)
+    contador+=1
+    if contador>10:
+        break
+
+cuenta=1
+while True:
+    print(cuenta)
+    cuenta+=1
+    if cuenta>10:
+        break
+print('Fin del programa')
