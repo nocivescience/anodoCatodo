13789424e2c5f9f0b3b47f538ddc30c79f0a2d52
ricardo <nocive.science@gmail.com>
2024-03-23 03:01:08 -0300

    terminado el asunto de while True por segunda vez

--- practicaWhile.py
+++ practicaWhile.py
@@ -1,14 +1,6 @@
-contador= 1
+count = 0
 while True:
-    print(contador)
-    contador+=1
-    if contador>10:
+    print(count)
+    count += 1
+    if count == 10:
         break
-
-cuenta=1
-while True:
-    print(cuenta)
-    cuenta+=1
-    if cuenta>10:
-        break
-print('Fin del programa')
